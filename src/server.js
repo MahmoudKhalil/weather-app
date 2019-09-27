865709f674b84bc4761a24cf3cef55b4dc8ff46e
MahmoudKhalil <mahmoudkhalil11@gmail.com>
2019-09-27 14:06:15 +0200

    Created server file and tested it, also added .gitignore file

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('This is to test if the server is up and running')
+})
+
+app.listen(3000, () => {
+    console.log('The server is up and running')
+})
